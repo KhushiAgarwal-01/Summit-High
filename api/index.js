480137ff23da4bb10ca23b17e85ae250aaa83254
Khushi Agarwal <ellipse012@gmail.com>
2024-06-24 02:06:59 +0530

    signup,signin ,authemtication

--- api/index.js
+++ api/index.js
@@ -1,6 +1,33 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from'dotenv';
+dotenv.config();
+import userRouter from "./routes/user.route.js"
+import authRouter from './routes/auth.route.js'
+
+mongoose.connect(process.env.MONGO)
+.then(()=>console.log("connected to db"))
+.catch((error)=>console.log({error:error}))
 
 const app=express();
+
+app.use(express.json());
+
+
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
+
+
+app.use((err,req,res,next)=>{
+const statusCode=err.statusCode||500;
+const message=err.message ||'Internal server error';
+return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message,
+})
+})
+
 
 app.listen(3000,()=>{
     console.log('server running on 3000')
